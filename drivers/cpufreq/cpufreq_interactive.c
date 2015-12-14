d63491053edf13731c18c3704afa9d7f8101f830
Todd Kjos <tkjos@google.com>
2015-12-14 15:51:18 -0800

    Add changes to non-sched files for EAS
    Change-Id: I38be889e84b0610285aeb231cb031e422279ee7d
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/cpufreq_interactive.c
    	include/linux/cputime.h
    	include/linux/preempt.h

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -38,6 +38,11 @@
 
 extern bool mdss_screen_on;
 
+#ifndef TJK_HMP
+ATOMIC_NOTIFIER_HEAD(migration_notifier_head);
+ATOMIC_NOTIFIER_HEAD(load_alert_notifier_head);
+#endif
+
 struct cpufreq_interactive_policyinfo {
 	struct timer_list policy_timer;
 	struct timer_list policy_slack_timer;
