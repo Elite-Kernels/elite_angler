06cf6f6e37a3f567feeeee388946580fb79398b9
Forrest Marble <forrestmarble@gmail.com>
2016-03-31 09:25:38 -0400

    script: more cleanup

--- packagesm/system/elite/Elite.sh
+++ packagesm/system/elite/Elite.sh
@@ -58,18 +58,6 @@ chmod 444 /dev/frandom
 echo "cfq" > /sys/block/mmcblk0/queue/scheduler
 echo "1536" > /sys/block/mmcblk0/queue/read_ahead_kb
 
-#Set CPU Min Frequencies
-echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-
-#Set CPU Max Frequencies
-echo 2649600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 2649600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-echo 2649600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-echo 2649600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-
 #Enable Zen Decision
 # stop mpdecision
 echo 0 > /sys/kernel/zen_decision/enabled
